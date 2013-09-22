f24ff065891a36ccbe2b73ca6d9ed168748a446c
Liam McLennan <liam.mclennan@gmail.com>
2013-09-22 10:40:45 +1000

    now loading scripts magically

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 var express = require('express'),
-    resourceserver = require('resourceserver');
+    resourceserver = require('resourceserver'),
+    path = require('path'),
+    shelljs = require('shelljs');
 
 var app = express();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function (req,res) {
-    res.render('index.html', function (err,html) {
+    res.render('index.html', {
+        js: js
+    }, function (err,html) {
         res.send(html);
     });
 });
 
-
 app.listen(3000);
 resourceserver.listen(3001);
+
+function js(dir) {
+    var fs = [];
+    shelljs.cd('public/');
+    shelljs.find(dir).filter(function (f) {
+        return f.match(/\.js$/);
+    }).map(function (f) {
+        fs.push('<script src="/' + f + '" ></script>');
+    });
+    return  fs.sort().join('\n');
+}
